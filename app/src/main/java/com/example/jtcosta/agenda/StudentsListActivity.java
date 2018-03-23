cd08bd8693d01cbcd8380dcfe58db9f897360f31
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-23 13:56:02 -0300

    Loading list data from activity

--- app/src/main/java/com/example/jtcosta/agenda/StudentsListActivity.java
+++ app/src/main/java/com/example/jtcosta/agenda/StudentsListActivity.java
@@ -1,7 +1,10 @@
 package com.example.jtcosta.agenda;
 
+import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StudentsListActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class StudentsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students_list);
+
+        String[] students = {"Daniel", "Ronald", "Jeff", "Phil"};
+        ListView studentsList = (ListView) findViewById(R.id.students_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
+        studentsList.setAdapter(adapter);
     }
 }
