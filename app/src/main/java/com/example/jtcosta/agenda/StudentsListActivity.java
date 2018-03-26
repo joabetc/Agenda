4ad7f182d6ce09792705d6562551483ed4f7a2dd
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-26 14:58:17 -0300

    Navigating to the form

--- app/src/main/java/com/example/jtcosta/agenda/StudentsListActivity.java
+++ app/src/main/java/com/example/jtcosta/agenda/StudentsListActivity.java
@@ -1,9 +1,12 @@
 package com.example.jtcosta.agenda;
 
 import android.app.LauncherActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class StudentsListActivity extends AppCompatActivity {
@@ -17,5 +20,15 @@ public class StudentsListActivity extends AppCompatActivity {
         ListView studentsList = (ListView) findViewById(R.id.students_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students);
         studentsList.setAdapter(adapter);
+
+        Button buttonNew = (Button) findViewById(R.id.button_new);
+        buttonNew.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intentGoToForm = new Intent(StudentsListActivity.this, FormActivity.class);
+                startActivity(intentGoToForm);
+            }
+        });
     }
 }
