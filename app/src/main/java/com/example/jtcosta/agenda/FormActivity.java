921784f6902975f154537c01177f833ceee6570e
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-26 15:17:10 -0300

    Replacing save button by options menu item

--- app/src/main/java/com/example/jtcosta/agenda/FormActivity.java
+++ app/src/main/java/com/example/jtcosta/agenda/FormActivity.java
@@ -2,6 +2,9 @@ package com.example.jtcosta.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,14 +15,23 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+    }
 
-        Button saveButton = (Button) findViewById(R.id.form_button_save);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(FormActivity.this, "Save button clicked!", Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_form, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_form_ok:
+                Toast.makeText(FormActivity.this, "Student saved!", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
