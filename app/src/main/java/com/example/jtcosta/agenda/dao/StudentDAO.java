366a7c8eb230309507bd4ce9c49f0590db5eda53
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-28 12:58:28 -0300

    Creating a constructor for DAO class

--- app/src/main/java/com/example/jtcosta/agenda/dao/StudentDAO.java
+++ app/src/main/java/com/example/jtcosta/agenda/dao/StudentDAO.java
@@ -1,8 +1,25 @@
 package com.example.jtcosta.agenda.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by jtcosta on 28/03/2018.
  */
 
-public class StudentDAO {
+public class StudentDAO extends SQLiteOpenHelper {
+    public StudentDAO(Context context) {
+        super(context, "Agenda", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
